4ac38b6b8925ea7f603ed3edad5f7cb2fde89ab7
Sandesh Sonel <sonelsandesh@gmail.com>
2026-01-04 11:19:43 +0530

    fix: time formate change

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -9,8 +9,12 @@ interface Props {
   children: ReactNode
 }
 
-const unixToTime = (ts: number) =>
-  new Date(ts * 1000).toLocaleTimeString('en-GB', { hour12: true })
+function unixToTime(ts: number) {
+  const d = new Date(ts * 1000)
+  return `${String(d.getHours()).padStart(2, '0')}:${String(
+    d.getMinutes()
+  ).padStart(2, '0')}:${String(d.getSeconds()).padStart(2, '0')}`
+}
 
 const AppLayout = ({ children }: Props) => {
   const {
